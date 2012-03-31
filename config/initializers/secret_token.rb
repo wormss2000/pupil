8501c1b7f607f2f9718895773ee96073c81cd3c6
Alex <1634344@gmail.com>
2012-03-31 12:08:14 +0400

    V 1.0.0.1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pupil::Application.config.secret_token = 'c641929f1b5fb3ff849615a8f3989e279b797a3ca2694e078f475ce08fc29cf15c5a39f3a5dc9c7921fd1d93aa43aa274fe2679a72f319ed5991b56427637a85'
